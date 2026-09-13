1afa71c4aeb2b7a2dcea6c713c0ec3788a062778
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-09-13 17:16:20 +0530

    phase3 transitgateway apply

--- versions.tf
+++ versions.tf
@@ -2,9 +2,29 @@ terraform {
   required_version = ">= 1.6.0"
 
   backend "s3" {
-    bucket = "harish-gaddam-bucket123"
+    bucket = "phase3-transit-gateway-terraform-state-434097521299"
     key    = "phase3/terraform.tfstate"
     region = "ap-south-1"
   }
 
-  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "hub"
+  region = var.aws_region
+
+  assume_role {
+    role_arn = var.hub_role_arn
+  }
+}
+
+provider "aws" {
+  alias  = "spoke"
+  region = var.aws_region
+}
