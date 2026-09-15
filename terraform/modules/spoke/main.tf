2f577e7560465d233857091d8459d42027d80d09
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-09-15 13:16:05 +0530

    phase3 tgw all changes

--- terraform/modules/spoke/main.tf
+++ terraform/modules/spoke/main.tf
@@ -1,18 +1,7 @@
-resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
-  transit_gateway_id = var.transit_gateway_id
-  vpc_id             = var.vpc_id
-  subnet_ids         = var.subnet_ids
+module "spoke_attachment" {
+  source = "../spoke-attachment"
 
-  dns_support  = "enable"
-  ipv6_support = "disable"
-
-  transit_gateway_default_route_table_association = true
-  transit_gateway_default_route_table_propagation = true
-
-  tags = merge(
-    var.tags,
-    {
-      Name = var.name
-    }
-  )
+  # required variables
+  # vpc_id             = var.vpc_id
+  # transit_gateway_id = var.transit_gateway_id
 }
