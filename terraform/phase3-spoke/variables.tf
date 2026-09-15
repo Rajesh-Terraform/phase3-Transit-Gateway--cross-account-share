66c9d4cb631ddb74c1843469f60a0215b39465de
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-09-15 13:51:05 +0530

    phase3 tgw all changes

--- terraform/phase3-spoke/variables.tf
+++ terraform/phase3-spoke/variables.tf
@@ -1,35 +1,46 @@
-variable "aws_region" {
-  description = "AWS region"
+variable "vpc_cidr" {
+  description = "Spoke VPC CIDR"
   type        = string
-  default     = "ap-south-1"
+  default     = "10.1.0.0/16"
+}
+
+variable "vpc_name" {
+  description = "Spoke VPC name"
+  type        = string
+  default     = "spoke-vpc"
+}
+
+variable "private_subnet_cidrs" {
+  description = "Spoke private subnet CIDRs"
+  type        = list(string)
+  default     = [
+    "10.1.1.0/24",
+    "10.1.2.0/24"
+  ]
+}
+
+variable "availability_zones" {
+  description = "Availability zones for Spoke subnets"
+  type        = list(string)
+  default     = [
+    "ap-south-1a",
+    "ap-south-1b"
+  ]
 }
 
 variable "transit_gateway_id" {
-  description = "Shared Transit Gateway ID"
+  description = "Transit Gateway ID"
   type        = string
-}
-
-variable "spoke_vpc_id" {
-  description = "Spoke VPC ID"
-  type        = string
-}
-
-variable "spoke_subnet_ids" {
-  description = "Spoke private subnet IDs used for TGW attachment"
-  type        = list(string)
 }
 
 variable "attachment_name" {
-  description = "Spoke TGW attachment name"
+  description = "TGW attachment name"
   type        = string
   default     = "spoke-tgw-attachment"
 }
 
 variable "tags" {
-  description = "Tags"
+  description = "Additional resource tags"
   type        = map(string)
-  default = {
-    Environment = "practice"
-    Project     = "network"
-  }
+  default     = {}
 }
