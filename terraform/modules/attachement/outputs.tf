0a9a5ba292137c676dff000627408512e843aa05
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-09-15 12:28:49 +0530

    phase3 tgw all changes

--- terraform/modules/attachement/outputs.tf
+++ terraform/modules/attachement/outputs.tf
@@ -1,7 +1,4 @@
 output "attachment_id" {
   value = aws_ec2_transit_gateway_vpc_attachment.this.id
 }
-
-output "attachment_state" {
-  value = aws_ec2_transit_gateway_vpc_attachment.this.state
-}          
+  
