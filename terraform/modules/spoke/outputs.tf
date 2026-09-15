a7f27f9aba605c19a52ac26746a261bfe7044eaf
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-09-15 12:25:15 +0530

    phase3 tgw all changes

--- terraform/modules/spoke/outputs.tf
+++ terraform/modules/spoke/outputs.tf
@@ -2,8 +2,3 @@ output "attachment_id" {
   description = "Transit Gateway VPC attachment ID"
   value       = aws_ec2_transit_gateway_vpc_attachment.this.id
 }
-
-output "attachment_state" {
-  description = "Transit Gateway VPC attachment state"
-  value       = aws_ec2_transit_gateway_vpc_attachment.this.state
-}  
