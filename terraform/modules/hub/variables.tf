abd0cc892cd98ec7610df443d680e373d70b2c95
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-09-23 11:02:38 +0530

    phase3 trasit gate way apply

--- terraform/modules/hub/variables.tf
+++ terraform/modules/hub/variables.tf
@@ -1,4 +1,15 @@
-variable "spoke_account_id" {
-  description = "AWS account ID of the Spoke"
+variable "transit_gateway_id" {
+  description = "Hub Transit Gateway ID"
   type        = string
-} 
+}
+
+variable "share_name" {
+  description = "RAM resource share name"
+  type        = string
+  default     = "phase3-tgw-share"
+}
+
+variable "spoke_account_id" {
+  description = "Spoke AWS account ID"
+  type        = string
+}  
