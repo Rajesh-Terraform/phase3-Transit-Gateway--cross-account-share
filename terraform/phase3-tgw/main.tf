7b3cd4e4f60aa481d4d7e3a2ce6db4f032da1c04
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-09-24 12:13:34 +0530

    phase3 all changes apply

--- terraform/phase3-tgw/main.tf
+++ terraform/phase3-tgw/main.tf
@@ -1,19 +1,5 @@
-resource "aws_ec2_transit_gateway" "this" {
-  description = "Transit Gateway for Hub and Spoke connectivity"
+module "spoke_tgw_attachment" {
+  source = "../modules/phase3-tgw-attachment"
 
-  amazon_side_asn                 = 64512
-  auto_accept_shared_attachments = "disable"
-
-  default_route_table_association = "enable"
-  default_route_table_propagation = "enable"
-
-  dns_support  = "enable"
-  vpn_ecmp_support = "enable"
-
-  tags = merge(
-    var.tags,
-    {
-      Name = var.tgw_name
-    }
-  )
-}
+  transit_gateway_id = var.transit_gateway_id
+}   
