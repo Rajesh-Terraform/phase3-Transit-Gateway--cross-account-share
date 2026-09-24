c2490651c794224544e0e7e0f0c3150ba6c27f2d
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-09-24 16:24:23 +0530

    phase3 all apply

--- terraform/phase3-hub/outputs.tf
+++ terraform/phase3-hub/outputs.tf
@@ -1,22 +1,19 @@
 output "transit_gateway_id" {
-  description = "Transit Gateway ID"
-  value       = module.tgw.transit_gateway_id
+  value = module.tgw.transit_gateway_id
 }
-
 
 output "transit_gateway_arn" {
-  description = "Transit Gateway ARN"
-  value       = module.tgw.transit_gateway_arn
+  value = module.tgw.transit_gateway_arn
 }
-
-
-output "ram_share_arn" {
-  description = "RAM resource share ARN"
-  value       = module.ram.share_arn
-}
-
 
 output "hub_attachment_id" {
-  description = "Hub VPC TGW attachment ID"
-  value       = module.hub_attachment.attachment_id
-}  
+  value = module.hub_attachment.attachment_id
+}
+
+output "ram_share_id" {
+  value = module.ram.resource_share_id
+}
+
+output "ram_share_arn" {
+  value = module.ram.resource_share_arn
+} 
