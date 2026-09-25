0a24de0bb6bbd9c10b6eabc13515c09f9f7489c5
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-09-25 11:15:55 +0530

    all changes done phase3

--- terraform/phase3-hub/provider.tf
+++ terraform/phase3-hub/provider.tf
@@ -1,14 +1,15 @@
-terraform {
-  required_version = ">= 1.5.0"
+module "tgw" {
+  source = "../modules/transit-gateway"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
+  tgw_name        = var.tgw_name
+  tgw_description = var.tgw_description
+  amazon_side_asn = var.amazon_side_asn
 }
 
-provider "aws" {
-  region = var.aws_region
+module "ram" {
+  source = "../modules/ram-share"
+
+  ram_share_name  = var.ram_share_name
+  tgw_arn         = module.tgw.transit_gateway_arn
+  spoke_account_id = var.spoke_account_id
 }   
